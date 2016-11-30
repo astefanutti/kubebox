d7fd72f2d85876d887ab2b6957ddd01ebc07f189
Antonin Stefanutti <antonin@stefanutti.fr>
2016-11-30 11:19:56 +0100

    Move cancellations code into a separate module

--- rubikube.js
+++ rubikube.js
@@ -7,50 +7,14 @@ const blessed  = require('blessed'),
       contrib  = require('blessed-contrib'),
       moment   = require('moment'),
       duration = require('moment-duration-format'),
+      task     = require('./lib/task'),
       screen   = blessed.screen();
-
-class Cancellations {
-
-  constructor() {
-    this.cancellations = {};
-  }
-
-  add(key, cancellation) {
-    const leaf = key.split('.').reduce((node, k) => {
-      if (!(k in node))
-        node[k] = [];
-      return node[k];
-    }, this.cancellations);
-    leaf.push(cancellation);
-  }
-
-  run(key) {
-    const node = key.split('.').reduce((node, k) => node[k], this.cancellations);
-    if (!node) return;
-    // we may want to run the cancellations in reverse order
-    node.forEach(cancellation => cancellation());
-    node.length = 0;
-    for (const child in node) {
-      if (Array.isArray(node[child])) {
-        this.run(key + '.' + child);
-      }
-    }
-  }
-
-  // TODO: use a dedicated widget to dump current tasks
-  toString(node = this.cancellations, key = 'cancellations') {
-    debug.log(`key: ${key}, isArray: ${Array.isArray(node)}`);
-    for (const child in node) {
-      this.toString(node[child], key + '.' + child);
-    }
-  }
-}
 
 const session = {
   access_token : null,
   namespace    : 'default',
   pods         : {},
-  cancellations: new Cancellations()
+  cancellations: new task.Cancellations()
 };
 
 // https://docs.openshift.org/latest/architecture/additional_concepts/authentication.html
