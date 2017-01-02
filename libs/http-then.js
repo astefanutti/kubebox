9ba65a987e695461eb8e6b093c0ac592902b9c3e
Antonin Stefanutti <antonin@stefanutti.fr>
2017-01-02 15:42:04 +0100

    EventEmitter style response callback

--- libs/http-then.js
+++ libs/http-then.js
@@ -34,62 +34,64 @@ function getStream(options, generator, async = true) {
   let request, clientAbort, serverAbort;
   const promise = new Promise((resolve, reject) => {
     const client = (options.protocol || 'http').startsWith('https') ? https : http;
-    request      = client.get(options, response => {
-      if (response.statusCode >= 400) {
-        const error    = new Error(`Failed to get resource ${options.path}, status code: ${response.statusCode}`);
-        // standard promises don't handle multi-parameters reject callbacks
-        error.response = response;
-        response.destroy(error);
-        return;
-      }
-      const gen = generator();
-      gen.next();
+    request      = client.get(options)
+      .on('error', reject)
+      .on('abort', () => clientAbort = true)
+      .on('response', response => {
+        if (response.statusCode >= 400) {
+          const error    = new Error(`Failed to get resource ${options.path}, status code: ${response.statusCode}`);
+          // standard promises don't handle multi-parameters reject callbacks
+          error.response = response;
+          response.destroy(error);
+          return;
+        }
+        const gen = generator();
+        gen.next();
 
-      response
-        .on('aborted', () => serverAbort = !clientAbort)
-        .on('data', chunk => {
-          const res = gen.next(chunk);
-          if (res.done) {
-            // we may work on the http.ClientRequest if needed
-            response.destroy();
-            response.body = res.value;
-            // ignored for async as it's already been resolved
-            resolve(response);
-          }
-        })
-        .on('end', () => {
-          if (serverAbort || clientAbort && !async) {
-            try {
-              // FIXME: what happens when the generator is done already?
-              const res = gen.throw(new Error('Request aborted'));
+        response
+          .on('aborted', () => serverAbort = !clientAbort)
+          .on('data', chunk => {
+            const res = gen.next(chunk);
+            if (res.done) {
+              // we may work on the http.ClientRequest if needed
+              response.destroy();
+              response.body = res.value;
+              // ignored for async as it's already been resolved
+              resolve(response);
+            }
+          })
+          .on('end', () => {
+            if (serverAbort || clientAbort && !async) {
+              try {
+                // FIXME: what happens when the generator is done already?
+                const res = gen.throw(new Error('Request aborted'));
+                // the generator may have already returned from the 'data' event
+                if (!async && !res.done) {
+                  response.body = res.value;
+                  resolve(response);
+                }
+              } catch (e) {
+                if (!async) {
+                  reject(e);
+                }
+                // else swallow for generators that ignore aborted requests
+              }
+            } else if (!(clientAbort && async)) {
+              const res = gen.next();
               // the generator may have already returned from the 'data' event
               if (!async && !res.done) {
                 response.body = res.value;
                 resolve(response);
               }
-            } catch (e) {
-              if (!async) {
-                reject(e);
-              }
-              // else swallow for generators that ignore aborted requests
             }
-          } else if (!(clientAbort && async)) {
-            const res = gen.next();
-            // the generator may have already returned from the 'data' event
-            if (!async && !res.done) {
-              response.body = res.value;
-              resolve(response);
-            }
-          }
-          // ignored if the generator is done already
-          gen.return();
-        });
+            // ignored if the generator is done already
+            gen.return();
+          });
 
-      if (async) {
-        resolve(response);
-      }
-    }).on('error', reject)
-      .on('abort', () => clientAbort = true)
+        if (async) {
+          resolve(response);
+        }
+      })
       .on('upgrade', (response, socket, head) => {
         // TODO: we may want to offer an API to pipe the socket
         if (response.statusCode !== 101) {
