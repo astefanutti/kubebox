f7d1e09b2d42c1ecabe214f13a2a87ed3f1bfd84
Antonin Stefanutti <antonin@stefanutti.fr>
2018-03-30 15:20:27 +0200

    In-cluster connection support

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 'use strict';
 
 const blessed = require('blessed'),
+      Context = require('./lib/config/context'),
+      fs      = require('fs'),
       Kubebox = require('./lib/kubebox');
 
 const screen = blessed.screen({
@@ -11,4 +13,18 @@ const screen = blessed.screen({
 
 screen.key(['q', 'C-c'], (ch, key) => process.exit(0));
 
-new Kubebox(screen);
+let server;
+
+if (process.env.KUBERNETES_SERVICE_HOST && process.env.KUBERNETES_SERVICE_PORT) {
+  const host = process.env.KUBERNETES_SERVICE_HOST;
+  const port = process.env.KUBERNETES_SERVICE_PORT;
+  const ca = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt');
+  const token = fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token');
+  server = Context.getBaseMasterApi(`https://${host}:${port}`);
+  server.ca = ca;
+  server.headers = {
+    Authorization : `Bearer ${token.toString('ascii')}`,
+  };
+}
+
+new Kubebox(screen, server);
