522373b256032a18b1f5254728634269c015b9fd
Antonin Stefanutti <antonin@stefanutti.fr>
2018-11-21 15:46:20 +0100

    Polish and rebuild

--- docs/kubebox.js
+++ docs/kubebox.js
@@ -601,12 +601,22 @@ module.exports = Namespace;
  *   user:
  *     client-certificate: path/to/my/client/cert
  *     client-key: path/to/my/client/key
+ * - name: orange-user
+ *   user:
+ *     auth-provider:
+ *       name: oidc
+ *       config:
+ *          client-id: xxx
+ *          client-secret: xxx
+ *          id-token: ...idtoken...
+ *          idp-issuer-url: https://myoidcprovider.com
+ *          refresh-token: ...refreshtoken...
  */
 class User {
 
   constructor({ name, token, username, password, 'client-certificate': certificatePath,
       'client-certificate-data': certificateBase64, 'client-key': keyPath,
-      'client-key-data': keyBase64 }) {
+      'client-key-data': keyBase64, 'auth-provider': authProvider }) {
     if (typeof name === 'undefined') {
       throw Error('User name must be defined!');
     }
@@ -614,6 +624,11 @@ class User {
     this.name = name;
     this.token = token;
     this.password = password;
+    if (typeof password === 'undefined' && typeof token === 'undefined') {
+      if (typeof authProvider !== 'undefined' && authProvider.name === 'oidc') {
+        this.token = authProvider.config['id-token'];
+      }
+    }
     this.certificatePath = certificatePath;
     this.certificateBase64 = certificateBase64;
     this.keyPath = keyPath;
@@ -629,6 +644,7 @@ class User {
 User.default = new User({ name: '', token: '' });
 
 module.exports = User;
+
 },{}],7:[function(require,module,exports){
 'use strict';
 
